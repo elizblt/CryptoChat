8cd25cd05f1ebe4a0de27ee118bf2f688e3bd1cd
Pablo ARRIEUMERLOU <p.arrieumerlou@gmail.com>
2024-04-02 22:52:33 +0200

    Configuration des workflows, de firebase et de l'appli

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCkW0dKGKVOr9Cp3anGEgyCYoAjfMPgr4A",
+        authDomain: "cryptochat-fae4d.firebaseapp.com",
+        projectId: "cryptochat-fae4d",
+        storageBucket: "cryptochat-fae4d.appspot.com",
+        messagingSenderId: "806610307339",
+        appId: "1:806610307339:web:ffcdc5fff493f2f2f3fa0e",
+        measurementId: "G-Z7GWYFZQL8"
+    }
+};
